9d7806e6cd12ab2519483ddec4e72d88432270f4
Matheus Felipe <matheus.reirasi@gmail.com>
2022-11-03 19:39:35 -0300

    load component when sending eth

--- client/src/components/Loader.jsx
+++ client/src/components/Loader.jsx
@@ -1,7 +1,7 @@
 const Loader = () => {
     return (
-        <h1>
-            Loader.
+        <h1 className="flex justify-center items-center py-3">
+            <div className="animate-spin rounded-full w-32 h-32 border-b-2"/>
         </h1>
     )
 }
