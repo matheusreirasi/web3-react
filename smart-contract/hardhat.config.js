d1d751749429d04b6b01d148510d3884b611d65f
Matheus Felipe <matheus.reirasi@gmail.com>
2022-11-07 12:43:51 -0300

    api keys are invisible

--- smart-contract/hardhat.config.js
+++ smart-contract/hardhat.config.js
@@ -1,6 +1,13 @@
-require("@nomicfoundation/hardhat-toolbox");
+require("@nomiclabs/hardhat-waffle")
 
-/** @type import('hardhat/config').HardhatUserConfig */
+const keys = require("./dev-keys.json")
+
 module.exports = {
-  solidity: "0.8.17",
-};
+  solidity:"0.8.9",
+  networks:{
+    goerli:{
+      url:"https://eth-goerli.g.alchemy.com/v2/" + keys.alchemyKey,
+      accounts: [keys.accountPrivateKey] //turn it private
+    }
+  }
+}
